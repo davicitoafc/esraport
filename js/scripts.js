3e645222389974f910fe313c33f4f194191d080e
David Cisneros <d.cisneros849@gmail.com>
2017-01-16 21:03:28 -0800

    added fixed navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,10 @@
 $(function() {
 
-/* Portfolio Section*/
-
-/* Photography */
-
-  $('#photographyDiv').animate({
-  width: '0px' // no width
-  }, 1000, function() {
-      $(this).hide();
-  });
-
+ /* Navbar transition */
   var waypoint = new Waypoint({
-    element: document.getElementById('portfolio'),
+    element: document.getElementById(''),
     handler: function(direction) {
-      console.log("fuck");
-      // Show and slide back open
-      $('#photographyDiv').show().animate({
-          width: '100%' // full width in pixels
-      }, 1000);
+
     },
     offset: -10
   })
